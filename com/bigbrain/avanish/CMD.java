5a774b06e127175922ed7c6f617910c3e285d584
Avanish <Avanish.E@Honeywell.com>
2025-01-08 15:27:16 +0100

    Added constans

---
+++ com/bigbrain/avanish/CMD.java
@@ -0,0 +1,21 @@
+package com.bigbrain.avanish;
+
+/**
+ * Allowed commands to be inputted as well as the error messages.
+ * @author uswup
+ */
+public final class CMD {
+    static final String ERROR = "Error";
+    static final String ERROR_MOVE_NOT_ALLOWED = "Error, move not allowed";
+    static final String QUIT = "quit";
+    static final String START = "start";
+    static final String PRINT = "print";
+    static final String PLACE = "place";
+    static final String PASS = "pass";
+    static final String MOVE = "move";
+
+
+    private CMD() {
+
+    }
+}
--- com/bigbrain/avanish/CommandHandler.java
+++ com/bigbrain/avanish/CommandHandler.java
@@ -12,9 +12,41 @@ public class CommandHandler {
         scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-        while (!Objects.equals(input, "QUIT")) {
-            //performCommand(input.split(" "));
+        while (!Objects.equals(input, CMD.QUIT)) {
+            performCommand(input.split(" "));
             input = scanner.nextLine();
+        }
+    }
+
+    /**
+     * Method with performs the inputted command/ calls the relevant method.
+     * @param currentCommand current inputted command
+     */
+    public static void performCommand(String[] currentCommand) {
+        if (currentCommand.length < 1) {
+            System.out.println(CMD.ERROR);
+            return;
+        }
+        switch (currentCommand[0]) {
+            case CMD.START:
+
+                break;
+
+            case CMD.PASS:
+
+                break;
+
+            case CMD.MOVE:
+
+                break;
+
+            case CMD.PLACE:
+
+                break;
+
+            case CMD.PRINT:
+
+                break;
         }
     }
 }
